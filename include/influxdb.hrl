c851f2428bd1c3f15189b2f1bb2da0b8e38e87fe
zhouzb <zhouzb@emqx.io>
2020-07-09 18:01:50 +0800

    feature(http): support writing datapoints to InfluxDB via http/https

---
+++ include/influxdb.hrl
@@ -0,0 +1,53 @@
+%%--------------------------------------------------------------------
+%% Copyright (c) 2020 EMQ Technologies Co., Ltd. All Rights Reserved.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%--------------------------------------------------------------------
+
+%% defaults
+-define(DEFAULT_SET_TIMESTAMP, true).
+-define(DEFAULT_PRECISION, ms).
+-define(DEFAULT_WRITE_PROTOCOL, udp).
+-define(DEFAULT_BATCH_SIZE, 32).
+-define(DEFAULT_UDP_OPTS, [{host, "127.0.0,1"},
+                           {port, 8089}]).
+-define(DEFAULT_HTTP_OPTS, [{host, "127.0.0.1"},
+                            {port, 8086},
+                            {database, "mydb"},
+                            {https_enabled, false}]).
+
+%% types
+-type precision() :: ns | u | ms | s | m | h.
+-type udp_opts() :: [udp_opt()].
+-type udp_opt() :: {host, inet:ip_address() | inet:hostname()}
+                 | {port, inet:port_number()}.
+-type http_opts() :: [http_opt()].
+-type http_opt() :: {host, inet:hostname()}
+                  | {port, inet:port_number()}
+                  | {database, string()}
+                  | {username, string()}
+                  | {password, string()}
+                  | {https_enabled, boolean()}
+                  | {ssl, ssloptions()}.
+-type ssloptions() :: [ssloption()].
+-type ssloption() :: {versions, [ssl:protocol_version()]}
+                   | {keyfile, file:filename()}
+                   | {certfile, file:filename()}
+                   | {cacertfile, file:filename()}
+                   | {dhfile, file:filename()}
+                   | {verify, verify_none | verify_peer}
+                   | {fail_if_no_peer_cert, boolean()}
+                   | {ciphers, string()}
+                   | {secure_renegotiate, boolean()}
+                   | {reuse_sessions, boolean() | save}
+                   | {honor_cipher_order, boolean()}.
--- src/influxdb.erl
+++ src/influxdb.erl
@@ -15,34 +15,46 @@
 -module(influxdb).
 -behaviour(gen_server).
 
--import(proplists, [get_value/3]).
+-include("influxdb.hrl").
 
 %% API.
--export([start_link/0, start_link/1]).
+-export([ start_link/0
+        , start_link/1
+        ]).
 
 %% gen_server.
--export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
-         code_change/3]).
+-export([ init/1
+        , handle_call/3
+        , handle_cast/2
+        , handle_info/2
+        , terminate/2
+        , code_change/3
+        ]).
 
 -export([ write/2
-        , write/3
-        , ping/1
-        , ping/2]).
+        , is_running/1
+        ]).
 
 -define(APP, influxdb).
 
--define(default_host, '127.0.0.1').
--define(default_port, 8089).
-
 -record(state, {
-    socket = undefined :: gen_udp:socket() | undefined,
+    set_timestamp = ?DEFAULT_SET_TIMESTAMP :: boolean(),
 
-    host = ?default_host :: inet:ip_address(),
+    precision = ?DEFAULT_PRECISION :: precision(),
 
-    port = ?default_port :: inet:port_number(),
+    batch_size = ?DEFAULT_BATCH_SIZE :: integer(),
 
-    set_timestamp = true :: boolean()
+    write_protocol = ?DEFAULT_WRITE_PROTOCOL :: http | udp,
+
+    udp_socket = undefined :: gen_udp:socket() | undefined,
+
+    udp_opts :: udp_opts(),
+
+    http_opts :: http_opts()
 }).
+
+-import(proplists, [ get_value/2
+                   , get_value/3]).
 
 %% API.
 
@@ -61,73 +73,97 @@ start_link(Opts) ->
                     fields := map(),
                     timestamp => integer()}).
 write(Pid, Points) ->
-    write(Pid, Points, []).
+    gen_server:call(Pid, {write, Points}).
 
--spec(write(Pid, Points, Options) -> ok | {erro, atom()}
-    when Pid :: pid(),
-         Points :: [Point] | Point,
-         Point :: #{measurement := atom() | binary() | list(),
-                    tags => map(),
-                    fields := map(),
-                    timestamp => integer()},
-         Options :: [Option],
-         Option :: {set_timestamp, boolean()}).
-write(Pid, Points, Options) ->
-    gen_server:call(Pid, {write, Points, Options}).
-
--spec(ping(pid()) -> ok | {erro, atom()}).
-ping(Pid) ->
-    ping(Pid, []).
-
--spec(ping(Pid, Options) -> ok | {erro, atom()}
-    when Pid :: pid(),
-         Options :: [Option],
-         Option ::  {ip, inet:ip_address()}
-                  | {port, inet:port_number()}).
-ping(Pid, Options) ->
-    gen_server:call(Pid, {ping, Options}).
+-spec(is_running(pid()) -> boolean()).
+is_running(Pid) ->
+    gen_server:call(Pid, is_running).
 
 % %% gen_server.
 
 init([Opts]) ->
-    Host = proplists:get_value(host, Opts, ?default_host),
-    case getaddr(Host) of
-        {ok, Addr} ->
-            {ok, Socket} = gen_udp:open(0, [binary, {active, false}, addr_family(Addr)]),
-            {ok, #state{host = Addr,
-                        socket = Socket,
-                        port = proplists:get_value(port, Opts, ?default_port),
-                        set_timestamp = proplists:get_value(set_timestamp, Opts, true)}};
-        {error, Reason} ->
-            {stop, Reason}
+    SetTimestamp = get_value(set_timestamp, Opts, ?DEFAULT_SET_TIMESTAMP),
+    Precision = get_value(precision, Opts, ?DEFAULT_PRECISION),
+    BatchSize = get_value(batch_size, Opts, ?DEFAULT_BATCH_SIZE),
+    WriteProtocol = get_value(write_protocol, Opts, ?DEFAULT_WRITE_PROTOCOL),
+    UDPOpts = merge_default_opts(get_value(udp, Opts, []), ?DEFAULT_UDP_OPTS),
+    HTTPOpts = merge_default_opts(get_value(http, Opts, []), ?DEFAULT_HTTP_OPTS),
+    Scheme = case get_value(https_enabled, HTTPOpts) of
+                 true -> "https";
+                 false -> "http"
+             end,
+    URL = Scheme ++ get_value(host, HTTPOpts) ++ ":" ++ get_value(port, HTTPOpts),
+    State = #state{set_timestamp = SetTimestamp,
+                   precision = Precision,
+                   batch_size = BatchSize,
+                   write_protocol = WriteProtocol,
+                   http_opts = maps:from_list([{url, URL} | HTTPOpts]),
+                   udp_socket = undefined},
+    case WriteProtocol of
+        udp ->
+            {ok, AddressFamily, IPAddress} = getaddr(get_value(host, UDPOpts)),
+            {ok, Socket} = gen_udp:open(0, [binary, {active, false}, AddressFamily]),
+            State#state{udp_opts = maps:put(host, IPAddress, maps:from_list(UDPOpts)),
+                        udp_socket = Socket};
+        http ->
+            State
     end.
 
-handle_call({write, Points, Options}, _From, State = #state{set_timestamp = SetTimestamp, 
-                                                            socket = Socket,
-                                                            host = Host,
-                                                            port = Port}) ->
-    LineOpts = [{set_timestamp, get_value(set_timestamp, Options, SetTimestamp)}],
-    case influxdb_line:encode(Points, LineOpts) of
-        {error, Reason} ->
-            {reply, {error, Reason}, State};
-        Data ->
-            {reply, gen_udp:send(Socket, Host, Port, Data), State}
-    end;
-
-handle_call({ping, Opts}, _From, State) ->
-    IP = inet:ntoa(proplist:get_value(host, Opts, {127, 0, 0, 1})),
-    Port = erlang:integer_to_list(proplist:get_value(port, Opts, 8086)),
-    case http_request(get, 
-                      {url("http://" ++ IP ++ ":" ++ Port ++ "/ping",[{"verbose", "true"}]), []}) of
-        {ok, Headers, _Body} ->
-            {reply, {ok, [{build_type, build_type(proplists:get_value("x-influxdb-build", Headers))},
-                          {version, proplists:get_value("x-influxdb-version", Headers)}]}, State};
-        {error, Reason} ->
-            {reply, {error, Reason}, State}
+handle_call(is_running, _From, State = #state{http_opts = HTTPOpts}) ->
+    URL = filename:join([get_value(url, HTTPOpts), "ping"]),
+    QueryParams = may_append_authentication_params([{"verbose", "true"}], HTTPOpts),    
+    HTTPOptions = case get_value(https_enabled, HTTPOpts) of
+                      false -> [{ssl, get_value(ssl, HTTPOpts)}];
+                      true -> []
+                  end,
+    case httpc_request(get, URL, QueryParams, [], <<>>, HTTPOptions) of
+        {ok, {{_, 200, _}, _, _}} -> {reply, true, State};
+        _ -> {reply, false, State}
     end;
 
 handle_call(_Request, _From, State) ->
 	{reply, ignored, State}.
+
+handle_cast({write, Points}, State = #state{set_timestamp = SetTimestamp,
+                                            precision = Precision,
+                                            write_protocol = WriteProtocol,
+                                            batch_size = BatchSize,
+                                            udp_socket = Socket,
+                                            udp_opts = UDPOpts,
+                                            http_opts = HTTPOpts}) ->
+    NPoints = drain_points(BatchSize - length(Points), Points),
+    LineOpts = [{set_timestamp, SetTimestamp},
+                {precision, Precision}],
+    case influxdb_line:encode(NPoints, LineOpts) of
+        {error, Reason} ->
+            logger:error("[InfluxDB] Encode ~p failed: ~p", [NPoints, Reason]);
+        Data ->
+            case WriteProtocol of
+                udp ->
+                    case gen_udp:send(Socket, get_value(ip_addr, UDPOpts), get_value(port, UDPOpts), Data) of
+                        {error, Reason} ->
+                            logger:error("[InfluxDB] Write ~p failed: ~p", [NPoints, Reason]);
+                        _ ->
+                            ok
+                    end;
+                http ->
+                    URL = filename:join([get_value(url, HTTPOpts), "write"]),
+                    QueryParams = may_append_authentication_params([{"db", get_value(database, HTTPOpts)}], HTTPOpts),                                              
+                    HTTPOptions = case get_value(https_enabled, HTTPOpts) of
+                                    false -> [{ssl, get_value(ssl, HTTPOpts)}];
+                                    true -> []
+                                end,
+                    case httpc_request(post, URL, QueryParams, [], Data, HTTPOptions) of
+                        {ok, {{_, 204, _}, _, _}} ->
+                            ok;
+                        {ok, {{_, StatusCode, ReasonPhrase}, _, Body}} ->
+                            logger:error("[InfluxDB] Write ~p failed: ~p ~s, Details: ~s", [NPoints, StatusCode, ReasonPhrase, Body]);
+                        {error, Reason} ->
+                            logger:error("[InfluxDB] Write ~p failed: ~p", [NPoints, Reason])
+                    end
+            end
+    end,
+    {noreply, State};
 
 handle_cast(_Msg, State) ->
 	{noreply, State}.
@@ -145,43 +181,57 @@ code_change(_OldVsn, State, _Extra) ->
 %% Internal functions
 %%====================================================================
 
+merge_default_opts(Opts, Default) when is_list(Opts) ->
+    merge_default_opts(maps:from_list(Opts), Default);
+merge_default_opts(Opts, []) ->
+    maps:to_list(Opts);
+merge_default_opts(Opts, [{K, V} | More]) ->
+    case maps:get(K, Opts, undefined) of
+        undefined -> merge_default_opts(Opts#{K => V}, More);
+        _ -> merge_default_opts(Opts, More)
+    end.
+
 getaddr(Host) ->
-    case inet:getaddr(Host, inet) of
-        {ok, Addr} -> {ok, Addr};
-        {error, _Reason} -> inet:getaddr(Host, inet6)
+    case getaddr(Host, inet) of
+        {ok, AddressFamily, IPAddress} ->
+            {ok, AddressFamily, IPAddress};
+        {error, _Reason} ->
+            getaddr(Host, inet6)
     end.
 
-addr_family({_, _, _, _}) -> inet;
-addr_family({_, _, _, _, _, _, _, _}) -> inet6.
-
-http_request(Method, Request) ->
-    parse_response(httpc:request(Method, Request, [], [])).
-
-url(Url, QueryParams) ->
-    NewUrl = lists:foldl(fun({K, V}, Acc) ->
-                    Acc ++ http_uri:encode(K) ++ "=" ++ http_uri:encode(V) ++ "&"
-                end, Url ++ "?", QueryParams),
-    case lists:last(NewUrl) of
-        "&" -> lists:droplast(NewUrl);
-        _ -> NewUrl
+getaddr(Host, AddressFamily)
+  when AddressFamily =:= inet orelse AddressFamily =:= inet6 ->
+    case inet:getaddr(Host, AddressFamily) of
+        {ok, IPAddress} ->
+            {ok, AddressFamily, IPAddress};
+        {error, Reason} ->
+            {error, Reason}
     end.
 
-parse_response({ok, {{_, Code, _}, Headers, Body}}) ->
-    parse_response({ok, Code, Headers, Body});
-parse_response({ok, {Code, Body}}) ->
-    parse_response({ok, Code, [], Body});
-parse_response({ok, 200, Headers, Body}) ->
-    {ok, Headers, Body};
-parse_response({ok, 201, Headers, Body}) ->
-    {ok, Headers, Body};
-parse_response({ok, 204, Headers, _Body}) ->
-    {ok, Headers, []};
-parse_response({ok, Code, Headers, Body}) ->
-    {error, {Code, Headers, Body}};
-parse_response({error, Reason}) ->
-    {error, Reason}.
+may_append_authentication_params(QueryParams0, AuthParams) ->
+    QueryParams = [{"u", get_value(username, AuthParams, undefined)},
+                   {"p", get_value(password, AuthParams, undefined)} | QueryParams0],
+    lists:dropwhile(fun({_, K}) -> K =:= undefined end, QueryParams).
 
-build_type("OSS") ->
-    open_source;
-build_type(_) ->
-    enterprise.
+httpc_request(Method, URL, QueryParams, Headers, Body, HTTPOptions) ->
+    NewURL = append_query_params_to_url(URL, QueryParams),
+    httpc:request(Method, {NewURL, Headers, "application/json", Body}, HTTPOptions, []).
+
+append_query_params_to_url(URL, QueryParams) ->
+    do_append_query_params_to_url(URL + "?", QueryParams).
+
+do_append_query_params_to_url(URL, [{K, V}]) ->
+    URL ++ http_uri:encode(K) ++ "=" ++ http_uri:encode(V);
+do_append_query_params_to_url(URL, [{K, V} | More]) ->
+    NewURL = URL ++ http_uri:encode(K) ++ "=" ++ http_uri:encode(V) ++ "&",
+    do_append_query_params_to_url(NewURL, More).
+
+drain_points(0, Acc) ->
+    lists:append(lists:reverse(Acc));
+drain_points(Cnt, Acc) ->
+    receive
+        {'$gen_cast', {write, Points}} ->
+            drain_points(Cnt - length(Points), [Points | Acc])
+    after 0 ->
+        lists:append(lists:reverse(Acc))
+    end.
